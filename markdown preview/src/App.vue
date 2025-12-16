32657a38c780f36026a6213b45a4ad8796aa2fc3
YunHui <805086021@qq.com>
2025-12-17 07:56:27 +0800

    DOWN

--- markdown preview/src/App.vue
+++ markdown preview/src/App.vue
@@ -1,11 +1,21 @@
 <template>
-    <div class="w-[100px] h-[100px] bg-red-300">123</div>
+  <div class="w-screen h-screen bg-red-300">
+    <MdEditor
+      v-model="text"
+      theme="light"
+      language="zh-CN"
+      :page-fullscreen="true"
+      previewTheme="mk-cute"
+    />
+  </div>
 </template>
-  
-<script setup lang='ts'>
-  
+
+<script setup>
+import { ref } from "vue";
+import { MdEditor } from "md-editor-v3";
+import "md-editor-v3/lib/style.css";
+
+const text = ref("# Hello Editor");
 </script>
-  
-<style>
-  
-</style>
+
+<style></style>
