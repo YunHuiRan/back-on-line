8927fdd76d369e4c912883b09df1972228ffde93
YunHui <805086021@qq.com>
2026-03-20 22:42:20 +0800

    chore

--- calculator/src/components/ButtonArea/ButtonRegularMode.vue
+++ calculator/src/components/ButtonArea/ButtonRegularMode.vue
@@ -1,29 +1,185 @@
 <template>
   <div class="w-full h-full grid p-2 gap-2 grid-cols-4">
-    <button value="ac" class="ac-button">ac</button>
-    <button value="del" class="del-button">del</button>
-    <button value="%" class="basic-operation-button">%</button>
-    <button value="÷" class="basic-operation-button">÷</button>
-    <button value="7" class="number-button">7</button>
-    <button value="8" class="number-button">8</button>
-    <button value="9" class="number-button">9</button>
-    <button value="×" class="basic-operation-button">×</button>
-    <button value="4" class="number-button">4</button>
-    <button value="5" class="number-button">5</button>
-    <button value="6" class="number-button">6</button>
-    <button value="-" class="basic-operation-button">-</button>
-    <button value="1" class="number-button">1</button>
-    <button value="2" class="number-button">2</button>
-    <button value="3" class="number-button">3</button>
-    <button value="+" class="basic-operation-button">+</button>
-    <button value="00" class="number-button">00</button>
-    <button value="0" class="number-button">0</button>
-    <button value="." class="dot-button">.</button>
-    <button value="=" class="equal-button">=</button>
+    <button
+      data-type="action"
+      value="ac"
+      class="ac-button"
+      @click="handleButtonClick"
+    >
+      ac
+    </button>
+    <button
+      data-type="action"
+      value="del"
+      class="del-button"
+      @click="handleButtonClick"
+    >
+      del
+    </button>
+    <button
+      data-type="operator"
+      value="%"
+      class="basic-operation-button"
+      @click="handleButtonClick"
+    >
+      %
+    </button>
+    <button
+      data-type="operator"
+      value="÷"
+      class="basic-operation-button"
+      @click="handleButtonClick"
+    >
+      ÷
+    </button>
+    <button
+      data-type="number"
+      value="7"
+      class="number-button"
+      @click="handleButtonClick"
+    >
+      7
+    </button>
+    <button
+      data-type="number"
+      value="8"
+      class="number-button"
+      @click="handleButtonClick"
+    >
+      8
+    </button>
+    <button
+      data-type="number"
+      value="9"
+      class="number-button"
+      @click="handleButtonClick"
+    >
+      9
+    </button>
+    <button
+      data-type="operator"
+      value="×"
+      class="basic-operation-button"
+      @click="handleButtonClick"
+    >
+      ×
+    </button>
+    <button
+      data-type="number"
+      value="4"
+      class="number-button"
+      @click="handleButtonClick"
+    >
+      4
+    </button>
+    <button
+      data-type="number"
+      value="5"
+      class="number-button"
+      @click="handleButtonClick"
+    >
+      5
+    </button>
+    <button
+      data-type="number"
+      value="6"
+      class="number-button"
+      @click="handleButtonClick"
+    >
+      6
+    </button>
+    <button
+      data-type="operator"
+      value="-"
+      class="basic-operation-button"
+      @click="handleButtonClick"
+    >
+      -
+    </button>
+    <button
+      data-type="number"
+      value="1"
+      class="number-button"
+      @click="handleButtonClick"
+    >
+      1
+    </button>
+    <button
+      data-type="number"
+      value="2"
+      class="number-button"
+      @click="handleButtonClick"
+    >
+      2
+    </button>
+    <button
+      data-type="number"
+      value="3"
+      class="number-button"
+      @click="handleButtonClick"
+    >
+      3
+    </button>
+    <button
+      data-type="operator"
+      value="+"
+      class="basic-operation-button"
+      @click="handleButtonClick"
+    >
+      +
+    </button>
+    <button
+      data-type="number"
+      value="00"
+      class="number-button"
+      @click="handleButtonClick"
+    >
+      00
+    </button>
+    <button
+      data-type="number"
+      value="0"
+      class="number-button"
+      @click="handleButtonClick"
+    >
+      0
+    </button>
+    <button
+      data-type="number"
+      value="."
+      class="dot-button"
+      @click="handleButtonClick"
+    >
+      .
+    </button>
+    <button
+      data-type="equal"
+      value="="
+      class="equal-button"
+      @click="handleButtonClick"
+    >
+      =
+    </button>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useCalculationFormulaStore } from "@/store/useCalculationFormulaStore";
+
+const calculationFormulaStore = useCalculationFormulaStore();
+
+function handleButtonClick(event: MouseEvent): void {
+  const target = event.target as HTMLElement;
+  const button = target.closest("button");
+  if (!button) return;
+
+  const value = button.getAttribute("value");
+  const type = button.getAttribute("data-type") || "";
+  if (!value) return;
+
+  calculationFormulaStore.addToRawString(value, type);
+}
+</script>
 
 <style scoped>
 button {
---
+++ calculator/src/components/ButtonArea/SliderToggle.vue
@@ -0,0 +1,36 @@
+<template>
+  <div
+    :class="[
+      'slider-container',
+      shifted ? '-translate-x-[50%]' : 'translate-x-0',
+    ]"
+  >
+    <span class="flex-center w-1/2 h-full"
+      ><slot name="left">{{ left }}</slot></span
+    >
+    <span class="flex-center w-1/2 h-full"
+      ><slot name="right">{{ right }}</slot></span
+    >
+  </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+  left: { type: String, default: "" },
+  right: { type: String, default: "" },
+  shifted: { type: Boolean, default: false },
+});
+</script>
+
+<style scoped>
+.slider-container {
+  width: 200%;
+  height: 100%;
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+  transition: translate 0.3s ease-in-out;
+}
+</style>
