efc13e6af251bab04c078fe184dbff73e8ec2ac2
YunHui <805086021@qq.com>
2026-03-20 22:46:02 +0800

    regular vfor

--- calculator/src/components/ButtonArea/ButtonRegularMode.vue
+++ calculator/src/components/ButtonArea/ButtonRegularMode.vue
@@ -1,164 +1,14 @@
 <template>
   <div class="w-full h-full grid p-2 gap-2 grid-cols-4">
     <button
-      data-type="action"
-      value="ac"
-      class="ac-button"
+      v-for="btn in buttons"
+      :key="btn.value + btn.type"
+      :data-type="btn.type"
+      :value="btn.value"
+      :class="btn.class"
       @click="handleButtonClick"
     >
-      ac
-    </button>
-    <button
-      data-type="action"
-      value="del"
-      class="del-button"
-      @click="handleButtonClick"
-    >
-      del
-    </button>
-    <button
-      data-type="operator"
-      value="%"
-      class="basic-operation-button"
-      @click="handleButtonClick"
-    >
-      %
-    </button>
-    <button
-      data-type="operator"
-      value="÷"
-      class="basic-operation-button"
-      @click="handleButtonClick"
-    >
-      ÷
-    </button>
-    <button
-      data-type="number"
-      value="7"
-      class="number-button"
-      @click="handleButtonClick"
-    >
-      7
-    </button>
-    <button
-      data-type="number"
-      value="8"
-      class="number-button"
-      @click="handleButtonClick"
-    >
-      8
-    </button>
-    <button
-      data-type="number"
-      value="9"
-      class="number-button"
-      @click="handleButtonClick"
-    >
-      9
-    </button>
-    <button
-      data-type="operator"
-      value="×"
-      class="basic-operation-button"
-      @click="handleButtonClick"
-    >
-      ×
-    </button>
-    <button
-      data-type="number"
-      value="4"
-      class="number-button"
-      @click="handleButtonClick"
-    >
-      4
-    </button>
-    <button
-      data-type="number"
-      value="5"
-      class="number-button"
-      @click="handleButtonClick"
-    >
-      5
-    </button>
-    <button
-      data-type="number"
-      value="6"
-      class="number-button"
-      @click="handleButtonClick"
-    >
-      6
-    </button>
-    <button
-      data-type="operator"
-      value="-"
-      class="basic-operation-button"
-      @click="handleButtonClick"
-    >
-      -
-    </button>
-    <button
-      data-type="number"
-      value="1"
-      class="number-button"
-      @click="handleButtonClick"
-    >
-      1
-    </button>
-    <button
-      data-type="number"
-      value="2"
-      class="number-button"
-      @click="handleButtonClick"
-    >
-      2
-    </button>
-    <button
-      data-type="number"
-      value="3"
-      class="number-button"
-      @click="handleButtonClick"
-    >
-      3
-    </button>
-    <button
-      data-type="operator"
-      value="+"
-      class="basic-operation-button"
-      @click="handleButtonClick"
-    >
-      +
-    </button>
-    <button
-      data-type="number"
-      value="00"
-      class="number-button"
-      @click="handleButtonClick"
-    >
-      00
-    </button>
-    <button
-      data-type="number"
-      value="0"
-      class="number-button"
-      @click="handleButtonClick"
-    >
-      0
-    </button>
-    <button
-      data-type="number"
-      value="."
-      class="dot-button"
-      @click="handleButtonClick"
-    >
-      .
-    </button>
-    <button
-      data-type="equal"
-      value="="
-      class="equal-button"
-      @click="handleButtonClick"
-    >
-      =
+      {{ btn.label ?? btn.value }}
     </button>
   </div>
 </template>
@@ -167,6 +17,36 @@
 import { useCalculationFormulaStore } from "@/store/useCalculationFormulaStore";
 
 const calculationFormulaStore = useCalculationFormulaStore();
+
+type ButtonDef = {
+  value: string;
+  type: string;
+  class: string;
+  label?: string;
+};
+
+const buttons: ButtonDef[] = [
+  { value: "ac", type: "action", class: "ac-button" },
+  { value: "del", type: "action", class: "del-button" },
+  { value: "%", type: "operator", class: "basic-operation-button" },
+  { value: "÷", type: "operator", class: "basic-operation-button" },
+  { value: "7", type: "number", class: "number-button" },
+  { value: "8", type: "number", class: "number-button" },
+  { value: "9", type: "number", class: "number-button" },
+  { value: "×", type: "operator", class: "basic-operation-button" },
+  { value: "4", type: "number", class: "number-button" },
+  { value: "5", type: "number", class: "number-button" },
+  { value: "6", type: "number", class: "number-button" },
+  { value: "-", type: "operator", class: "basic-operation-button" },
+  { value: "1", type: "number", class: "number-button" },
+  { value: "2", type: "number", class: "number-button" },
+  { value: "3", type: "number", class: "number-button" },
+  { value: "+", type: "operator", class: "basic-operation-button" },
+  { value: "00", type: "number", class: "number-button" },
+  { value: "0", type: "number", class: "number-button" },
+  { value: ".", type: "number", class: "dot-button" },
+  { value: "=", type: "equal", class: "equal-button" },
+];
 
 function handleButtonClick(event: MouseEvent): void {
   const target = event.target as HTMLElement;
